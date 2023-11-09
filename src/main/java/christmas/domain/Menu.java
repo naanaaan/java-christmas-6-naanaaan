588a63d3317b5c13919e62769b45a33daacb955b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-09 18:53:45 +0900

    feat(Menu): 메뉴 객체 추가

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,45 @@
+package christmas.domain;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+
+public enum Menu {
+
+	MUSHROOM_SOUP(FoodCategory.APPETIZER, "양송이수프", 6_000),
+	TAPAS(FoodCategory.APPETIZER, "타파스", 5_500),
+	CAESAR_SALAD(FoodCategory.APPETIZER, "시저샐러드", 8_000),
+
+	T_BONE_STEAK(FoodCategory.MAIN, "티본스테이크", 55_000),
+	BBQ_RIBS(FoodCategory.MAIN, "바비큐립", 54_000),
+	SEAFOOD_PASTA(FoodCategory.MAIN, "해산물파스타", 35_000),
+	CHRISTMAS_PASTA(FoodCategory.MAIN, "크리스마스파스타", 25_000),
+
+	CHOCO_CAKE(FoodCategory.DESSERT, "초코케이크", 15_000), 
+	ICE_CREAM(FoodCategory.DESSERT, "아이스크림", 5_000),
+
+	ZERO_COLA(FoodCategory.BEVERAGE, "제로콜라", 3_000),
+	RED_WINE(FoodCategory.BEVERAGE, "레드와인", 60_000),
+	CHAMPAGNE(FoodCategory.BEVERAGE, "샴페인", 25_000);
+
+	private final FoodCategory category;
+	private final String name;
+	private final int price;
+
+	Menu(FoodCategory category, String name, int price) {
+		this.category = category;
+		this.name = name;
+		this.price = price;
+	}
+
+	public FoodCategory getCategory() {
+		return category;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+}
