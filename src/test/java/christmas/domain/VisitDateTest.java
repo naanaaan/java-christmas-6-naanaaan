3ad45c09aab97cf9ff2ff5c6440110b98bd9fc80
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-09 21:42:49 +0900

    test(VisitDate): 방문날짜가 1 ~ 31을 벗어났을 때 예외처리 기능 테스트 코드 작성

---
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class VisitDateTest {
+
+	@DisplayName("방문날짜가 해당 월의 최대 일을 초과할 때 예외가 발생한다.")
+	@ValueSource(ints = { 32, 34, 36 })
+	@ParameterizedTest
+	void createVisitDateByOverRange(int day) {
+		assertThatThrownBy(() -> new VisitDate(day))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@DisplayName("방문날짜가 해당 월의 일에 해당될 때 정상작동한다.")
+	@ValueSource(ints = { 1, 24, 31 })
+	@ParameterizedTest
+	void checkNomalOperation1(int day) {
+		assertDoesNotThrow(() -> new VisitDate(day));
+	}
+}
