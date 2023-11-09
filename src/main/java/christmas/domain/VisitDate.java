5e96c59aa2a54eab4074c02613a434aa868ff94b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-09 21:40:22 +0900

    feat(VisitDate): 방문날짜가 1 ~ 31을 벗어났을 때 예외처리 기능 추가

---
+++ src/main/java/christmas/domain/VisitDate.java
@@ -0,0 +1,28 @@
+package christmas.domain;
+
+import java.time.LocalDate;
+
+public class VisitDate {
+
+	private static int YEAR = 2023;
+	private static int MONTH = 12;
+	private static int MIN_DAY = 1;
+	private static int MAX_DAY = LocalDate.of(YEAR, MONTH, 1).lengthOfMonth();
+	
+	private final LocalDate date;
+
+	public VisitDate(int day) {
+		validate(day);
+		this.date = LocalDate.of(YEAR, MONTH, day);
+	}
+
+	private void validate(int day) {
+		validateisOutOfRange(day);
+	}
+
+	private void validateisOutOfRange(int day) {		
+		if (day > MAX_DAY || day < MIN_DAY) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
