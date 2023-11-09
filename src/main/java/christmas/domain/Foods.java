c91d14b4fd251d7f60f0a187589081099272a0b2
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-09 19:04:22 +0900

    feat(Foods): 메뉴의 갯수 예외처리 기능 추가

---
+++ src/main/java/christmas/domain/Foods.java
@@ -0,0 +1,35 @@
+package christmas.domain;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Foods {
+
+	public static final int MIN_NUMBER = 1;
+	public static final int MAX_NUMBER = 20;
+
+	private final List<Food> foods;
+
+	public Foods(List<Food> foods) {
+		validate(foods);
+		this.foods = foods;
+	}
+
+	private void validate(List<Food> foods) {
+		validateFoodNumber(foods);
+	}
+
+	private void validateFoodNumber(List<Food> foods) {
+		if (isOutOfRange(foods.size())) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private boolean isOutOfRange(int size) {
+		return size > MAX_NUMBER || size < MIN_NUMBER;
+	}
+
+	public List<Food> getFoods() {
+		return Collections.unmodifiableList(foods);
+	}
+}
