99350d7d57492e99a7150bc347d6adfd4388475e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-09 18:53:04 +0900

    feat(FoodCategory): 음식 카테고리 객체 추가

---
+++ src/main/java/christmas/domain/FoodCategory.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public enum FoodCategory {
+
+    APPETIZER("에피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    BEVERAGE("음료");
+
+	private final String categoryName;
+
+	FoodCategory(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getKoreanName() {
+		return categoryName;
+	}
+}
