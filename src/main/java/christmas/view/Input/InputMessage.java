d7fa97bf3de88c79289df7e5dd2fff85f9be4584
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 20:57:09 +0900

    feat(InputMessage): 입력 메세지 추가

---
+++ src/main/java/christmas/view/Input/InputMessage.java
@@ -0,0 +1,17 @@
+package christmas.view.Input;
+
+public enum InputMessage {
+	
+	INPUT_VISIT_DATE("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"),
+	INPUT_MENUS("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+	private final String message;
+
+	InputMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
