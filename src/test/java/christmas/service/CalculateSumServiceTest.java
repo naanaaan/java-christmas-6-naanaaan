63ee94faaf917e9ad44fa42112793e5ac58a0c1e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 17:12:13 +0900

    test(CalculateSumService): 합계를 구하는 기능 테스트 코드 작성

---
+++ src/test/java/christmas/service/CalculateSumServiceTest.java
@@ -0,0 +1,24 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculateSumServiceTest {
+
+	private CalculateSumService calculateSumService;
+
+	@BeforeEach
+	void setUp() {
+		calculateSumService = new CalculateSumService();
+	}
+
+	@DisplayName("합계를 확인한다.")
+	@Test
+	void checkSum() {
+		int result = calculateSumService.calculateSum(-1_000, -2_023, -2400);
+		assertEquals(result, -5423);
+	}
+}
