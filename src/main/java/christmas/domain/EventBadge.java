c52317080ac25062974189203c35005ac8147dff
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 18:23:44 +0900

    feat(EventBadge): 총 혜택 금액에 따른 배지를 구하는 기능 추가

---
+++ src/main/java/christmas/domain/EventBadge.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum EventBadge {
+
+	SANTA(20000, "산타"), 
+	TREE(10000, "트리"),
+	STAR(5000, "별"),
+	NOTHING(0, "없음");
+
+	private final int minTotalBenefitsAmount;
+	private final String badgeName;
+
+	EventBadge(int minTotalBenefitsAmount, String badgeName) {
+		this.minTotalBenefitsAmount = minTotalBenefitsAmount;
+		this.badgeName = badgeName;
+	}
+
+	public static EventBadge getBadgeByTotalBenefitsAmount(int totalBenefitsAmount) {
+		return Arrays.stream(values())
+				.filter(badge -> totalBenefitsAmount >= badge.minTotalBenefitsAmount)
+				.findFirst()
+				.orElse(NOTHING);
+	}
+
+	public String getBadgeName() {
+		return badgeName;
+	}
+}
