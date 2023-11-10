513bb2d3598bea3d2529e5e8f0b90ae60ab3d6b4
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 16:16:41 +0900

    refactor(DDayDiscountService): discount메소드 리턴값 음수로 변경

--- src/main/java/christmas/service/DDayDiscountService.java
+++ src/main/java/christmas/service/DDayDiscountService.java
@@ -5,13 +5,13 @@ public class DDayDiscountService {
 	private static final int MIN_DAY = 1;
 	private static final int MAX_DAY = 25;
 	private static final int D_DAY_DISCOUNT = 100;
-	private static final int MAX_DISCOUNT_AMOUNT = 2400;
-	private static final int D_DAY_BASIC_DISCOUNT_AMOUNT = 1000;
+	private static final int MAX_DISCOUNT_AMOUNT = 2_400;
+	private static final int D_DAY_BASIC_DISCOUNT_AMOUNT = 1_000;
 
 	public int discount(int day) {
 		int discountAmount = D_DAY_BASIC_DISCOUNT_AMOUNT + calculateAdditionalDiscount(day);
 
-		return -discountAmount;
+		return discountAmount * -1;
 	}
 
 	private int calculateAdditionalDiscount(int day) {
