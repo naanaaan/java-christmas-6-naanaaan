a71532e9ed6d12cd92ace7ea1729e70db09c9a6b
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 16:19:49 +0900

    test(SpecialDiscountService): 특별 할인금액을 구하는 기능 테스트 코드 작성

---
+++ src/test/java/christmas/service/SpeciallDiscountServiceTest.java
@@ -0,0 +1,27 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SpeciallDiscountServiceTest {
+
+	private SpecialDiscountService specialDiscountService;
+
+	@BeforeEach
+	void setUp() {
+		specialDiscountService = new SpecialDiscountService();
+	}
+
+	@DisplayName("특별 할인 날짜일 때 할인금액을 확인한다.")
+	@CsvSource(value = { "3,-1000", "17,-1000", "31,-1000" }, delimiter = ',')
+	@ParameterizedTest
+	void checkDiscountAmount(int day, int expect) {
+		int discountAmount = specialDiscountService.discount(day);
+
+		assertEquals(discountAmount, expect);
+	}
+}
