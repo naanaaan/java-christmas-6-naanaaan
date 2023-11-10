a65408e449b7db47c473ea42c3d6c2a9cec6cc99
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 10:00:32 +0900

    feat(DDAYDiscountService): 디데이 할인 금액을 구하는 기능 추가

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -35,4 +35,8 @@ public class VisitDate {
 
 		return false;
 	}
+
+	public int getDay() {
+		return date.getDayOfMonth();
+	}
 }
---
+++ src/main/java/christmas/service/DDayDiscountService.java
@@ -0,0 +1,24 @@
+package christmas.service;
+
+public class DDayDiscountService {
+
+	private static final int MIN_DAY = 1;
+	private static final int MAX_DAY = 25;
+	private static final int D_DAY_DISCOUNT = 100;
+	private static final int MAX_DISCOUNT_AMOUNT = 2400;
+	private static final int D_DAY_BASIC_DISCOUNT_AMOUNT = 1000;
+
+	public int dDayDiscount(int day) {
+		int discountAmount = D_DAY_BASIC_DISCOUNT_AMOUNT + calculateAdditionalDiscount(day);
+
+		return -discountAmount;
+	}
+
+	private int calculateAdditionalDiscount(int day) {
+		if (day >= MIN_DAY && day <= MAX_DAY) {
+			return ((day - MIN_DAY) * D_DAY_DISCOUNT);
+		}
+
+		return MAX_DISCOUNT_AMOUNT;
+	}
+}
