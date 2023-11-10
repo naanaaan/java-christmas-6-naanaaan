0d98dab66f2be2932edb8b3b924143b12837ebe9
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 17:11:56 +0900

    feat(CalculateSumService): 합계를 구하는 기능 추가

---
+++ src/main/java/christmas/service/CalculateSumService.java
@@ -0,0 +1,16 @@
+package christmas.service;
+
+import java.util.Arrays;
+
+public class CalculateSumService {
+
+	public int calculateSum(Integer... integers) {
+		if (integers == null || integers.length == 0) {
+			return 0;
+		}
+
+		return Arrays.stream(integers)
+				.mapToInt(Integer::intValue)
+				.sum();
+	}
+}
