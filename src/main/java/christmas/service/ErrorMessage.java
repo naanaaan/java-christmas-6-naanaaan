728a0acaf3c3b48f1c466903e1b8e6da1e75bd79
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-11 03:32:33 +0900

    feat(ErrorMessage): 에러메세지 추가

---
+++ src/main/java/christmas/service/ErrorMessage.java
@@ -0,0 +1,17 @@
+package christmas.service;
+
+public enum ErrorMessage {
+	VISITDATE("유효하지 않은 날짜입니다. 다시 입력해 주세요."), 
+	ORDER_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+	PREFIX("[ERROR] ");
+
+	private final String message;
+
+	ErrorMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return PREFIX.message + message;
+	}
+}
