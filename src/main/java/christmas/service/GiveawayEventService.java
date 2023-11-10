2dd7d30e692709b7c202936c959faf92ee472889
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 16:56:22 +0900

    feat(GiveawayEventService): 증정품 증정과 증정품의 가격을 구하는 기능 추가

---
+++ src/main/java/christmas/service/GiveawayEventService.java
@@ -0,0 +1,25 @@
+package christmas.service;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import christmas.domain.Food;
+import christmas.domain.Menu;
+
+public class GiveawayEventService {
+
+	private static final Food EVENT_PRODUCT = Menu.CHAMPAGNE.toFood();
+	private static final int GIVE_COUNT = 1;
+
+	public List<Food> giveEventProduct() {
+		return Stream.generate(() -> EVENT_PRODUCT)
+				.limit(GIVE_COUNT)
+				.toList();
+	}
+
+	public int calculateSumEventProductPrice(List<Food> eventProducts) {
+		return eventProducts.stream()
+				.mapToInt(food -> food.price())
+				.sum() * -1;
+	}
+}
