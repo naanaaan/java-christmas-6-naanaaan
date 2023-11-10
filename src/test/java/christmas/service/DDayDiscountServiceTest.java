6e9285d33dc2319fab32370793a9e610c46fa121
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-10 10:00:46 +0900

    test(DDayDiscountService): 디데이 할인 금액을 구하는 기능 테스트 코드 작성

---
+++ src/test/java/christmas/service/DDayDiscountServiceTest.java
@@ -0,0 +1,27 @@
+package christmas.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class DDayDiscountServiceTest {
+	
+	private DDayDiscountService dDayDiscountService;
+	
+	@BeforeEach
+	void setUp() {
+		dDayDiscountService = new DDayDiscountService();
+	}
+	
+	@DisplayName("test")
+	@CsvSource(value = {"1,-1000", "26,-3400", "25, -3400", "20, -2900"}, delimiter = ',')
+	@ParameterizedTest
+	void test(int day, int expect) {
+		int discountAmount = dDayDiscountService.dDayDiscount(day);
+		
+		assertEquals(discountAmount, expect);
+	}
+}
