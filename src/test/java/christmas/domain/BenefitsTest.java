31aeface9a3037c09cb0669cc6c9e73d18d8e9c5
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-11 23:42:32 +0900

    test(Benefits): 할인가격이 0이 아닌 혜택들을 반환하는 기능 테스트 코드 작성

---
+++ src/test/java/christmas/domain/BenefitsTest.java
@@ -0,0 +1,29 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BenefitsTest {
+
+	@DisplayName("할인가격이 0이 아닌 혜택들을 반환하는지 확인한다.")
+	@Test
+	void checkNotZeroDiscountPriceBenefits() {
+		Benefit benefit1 = new Benefit(Event.DDAY_DISCOUNT, -1000);
+		Benefit benefit2 = new Benefit(Event.WEEKDAY_DISCOUNT, 0);
+		Benefit benefit3 = new Benefit(Event.GIVEAWAY_EVENT, -10000);
+
+		Benefits benefitsObject = new Benefits(List.of(benefit1, benefit2, benefit3));
+
+		List<Benefit> result = benefitsObject.getNotZeroDiscountPrice();
+
+		assertEquals(2, result.size());
+		assertTrue(result.contains(benefit1));
+		assertTrue(result.contains(benefit3));
+
+	}
+}
