902d2b6f38275304ebc264cd474d0f52cb47e2a5
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 07:08:08 +0900

    test(VisitDateService): 방문 날짜를 생성하는 기능 테스트 코드 잓어

---
+++ src/test/java/christmas/service/visitDate/VisitDateServiceTest.java
@@ -0,0 +1,31 @@
+package christmas.service.visitDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import christmas.domain.VisitDate;
+
+class VisitDateServiceTest {
+
+	private VisitDateService visitDateService;
+
+	@BeforeEach
+	void setUp() {
+		visitDateService = new VisitDateService();
+	}
+
+	@Test
+	@DisplayName("방문 날짜의 일자가 주어진 날짜와 동일한지 확인한다.")
+	void checkDayOfVisitDateSameAsDay() {
+		int day = 25;
+
+		VisitDate visitDate = visitDateService.createVisitDate(day);
+		
+		assertNotNull(visitDate);
+		assertEquals(day, visitDate.getDay());
+	}
+}
