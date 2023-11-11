e760b3c07a04870a4d31b66fbc03ed03a9588368
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 02:38:00 +0900

    test(Menu): 메뉴 이름에 해당하는 Menu를 가져오는 기능 테스트 코드 추가

---
+++ src/test/java/christmas/domain/MenuTest.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class MenuTest {
+
+	@DisplayName("메뉴 이름에 해당하는 Enum 객체를 확인한다.")
+	@CsvSource({ "양송이수프, MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA", "초코케이크, CHOCO_CAKE",
+			"아이스크림, ICE_CREAM", "제로콜라, ZERO_COLA", })
+	@ParameterizedTest
+	void checkGetMenuByName(String menuName, Menu expect) {
+		Optional<Menu> menu = Menu.getMenuByName(menuName);
+
+		if (menu.isPresent()) {
+			assertEquals(expect, menu.get());
+			assertTrue(menu.get().getName().equals(menuName));
+		}
+	}
+}
