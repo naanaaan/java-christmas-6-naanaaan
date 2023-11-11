313ac42bd7a9bdc051a92d0d80014517a0522ce6
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-11 15:26:35 +0900

    refactor(FoodServiceTest): 삭제된 예외처리의 테스트 코드 삭제

--- src/test/java/christmas/service/food/FoodServiceTest.java
+++ src/test/java/christmas/service/food/FoodServiceTest.java
@@ -1,7 +1,5 @@
 package christmas.service.food;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
@@ -9,9 +7,6 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import christmas.domain.Foods;
 import christmas.domain.Menu;
@@ -33,23 +28,5 @@ public class FoodServiceTest {
 		int expect = 104_000;
 
 		assertEquals(priceSum, expect);
-	}
-
-	@DisplayName("음식들을 생성하고 사이즈를 확인한다.")
-	@CsvSource(value = { "타파스-1,제로콜라-1;2", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1;5" }, delimiter = ';')
-	@ParameterizedTest
-	void testCreateFoodsForCheckFoodsSize(String foodsAndNumbers, int expect) {
-		assertDoesNotThrow(() -> {
-			Foods foods = foodService.createFoods(foodsAndNumbers);
-			assertEquals(foods.getFoodsSize(), expect);
-		});
-	}
-
-	@DisplayName("음식이름을 중복되게 입력할 시 예외가 발생한다.")
-	@ValueSource(strings = { "시저샐러드-1,시저샐러드-1", "타파스-1,타파스-2,제로콜라-1" })
-	@ParameterizedTest
-	void testCreateFoodsForCheckDuplicateFoodNameException(String foodsAndNumbers) {
-		assertThatThrownBy(() -> foodService.createFoods(foodsAndNumbers))
-				.isInstanceOf(IllegalArgumentException.class);
 	}
 }
