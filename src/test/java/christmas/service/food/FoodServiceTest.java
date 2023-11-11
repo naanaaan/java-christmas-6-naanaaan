95efd4238bd20a3823c4209a2daa035d6769195e
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 04:20:20 +0900

    test(FoodServiceTest): 입력값에 따라 음식들을 생성하는 기능 테스트 코드 작성

--- src/test/java/christmas/service/food/FoodServiceTest.java
+++ src/test/java/christmas/service/food/FoodServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import christmas.domain.FoodCategory;
 import christmas.domain.Foods;
 import christmas.domain.Menu;
 
@@ -21,12 +22,24 @@ public class FoodServiceTest {
 		foods = new Foods(List.of(Menu.BBQ_RIBS.toFood(), Menu.CHAMPAGNE.toFood(), Menu.CHRISTMAS_PASTA.toFood()));
 	}
 
+	@DisplayName("입력값에 따라 생성되는 음식들을 확인한다.")
+	@Test
+	void checkCreateFoodsByInputValue() {
+		String inputValue = "바비큐립-1,샴페인-1,크리스마스파스타-1";
+		Foods foods = foodService.createFoods(inputValue);
+
+		assertEquals(2, foods.countFoodsByCategory(FoodCategory.MAIN));
+		assertEquals(1, foods.countFoodsByCategory(FoodCategory.BEVERAGE));
+		assertEquals(0, foods.countFoodsByCategory(FoodCategory.APPETIZER));
+	}
+
 	@DisplayName("음식들의 총 가격의 합을 확인한다.")
 	@Test
 	void checkFoodsPriceSum() {
 		int priceSum = foodService.calculateFoodsPriceSum(foods);
 		int expect = 104_000;
 
-		assertEquals(priceSum, expect);
+		assertEquals(expect, priceSum);
 	}
+
 }
