d62108e52f056ca09edeaf66b99de51a55038043
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-11 22:44:56 +0900

    refactor(EventBadgeTest): getBadgeByTotalBenefitsPrice메소드 수정 후 테스트 코드 수정

--- src/test/java/christmas/domain/EventBadgeTest.java
+++ src/test/java/christmas/domain/EventBadgeTest.java
@@ -2,18 +2,25 @@ package christmas.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class EventBadgeTest {
-	
+
 	@DisplayName("총혜택 금애")
-	@CsvSource(value = {"6000,별", "11000,트리", "4000,없음", "20000,산타", "25000,산타"}, delimiter = ',')
+	@CsvSource(value = { "-6000,별", "11000,트리", "-20000,산타", "25000,산타" }, delimiter = ',')
 	@ParameterizedTest
-	void checkBadgeByTotalBenefitsAmount(int amount, String expect) {
-		EventBadge badge = EventBadge.getBadgeByTotalBenefitsAmount(amount);
-		
-		assertEquals(badge.getBadgeName(), expect);
+	void checkBadgeByTotalBenefitsAmount(int totalBenefitsPrice, String expect) {
+		Optional<EventBadge> badge = EventBadge.getBadgeByTotalBenefitsPrice(totalBenefitsPrice);
+		String badgeName = "";
+
+		if (badge.isPresent()) {
+			badgeName = badge.get().getBadgeName();
+		}
+
+		assertEquals(badgeName, expect);
 	}
 }
