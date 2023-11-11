e966b05850639247e79849fa96142c56f3da0213
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 02:48:11 +0900

    refactor(MenuTest): @CsvSource에 value와 delimiter 명시

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -12,8 +12,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class MenuTest {
 
 	@DisplayName("메뉴 이름에 해당하는 Enum 객체를 확인한다.")
-	@CsvSource({ "양송이수프, MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA", "초코케이크, CHOCO_CAKE",
-			"아이스크림, ICE_CREAM", "제로콜라, ZERO_COLA", })
+	@CsvSource(value = { "양송이수프, MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA",
+			"초코케이크, CHOCO_CAKE", "아이스크림, ICE_CREAM", "제로콜라, ZERO_COLA" }, delimiter = ',')
 	@ParameterizedTest
 	void checkGetMenuByName(String menuName, Menu expect) {
 		Optional<Menu> menu = Menu.getMenuByName(menuName);
