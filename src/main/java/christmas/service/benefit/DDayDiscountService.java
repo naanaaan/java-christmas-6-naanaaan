b839bff2facb18e614a12d276d065d13ecc3f6f0
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 03:51:20 +0900

    fix(DDayDiscountService): 디데이할인 25일 이후 적용되는 버그 수정

--- src/main/java/christmas/service/benefit/DDayDiscountService.java
+++ src/main/java/christmas/service/benefit/DDayDiscountService.java
@@ -9,21 +9,19 @@ public class DDayDiscountService {
 	private static final int MIN_DAY = 1;
 	private static final int MAX_DAY = 25;
 	private static final int D_DAY_DISCOUNT = 100;
-	private static final int MAX_DISCOUNT_AMOUNT = 2_400;
 	private static final int D_DAY_BASIC_DISCOUNT_AMOUNT = 1_000;
 
 	public Benefit getBenefit(int day) {
-		int benefitAmount = 0;
-		benefitAmount = D_DAY_BASIC_DISCOUNT_AMOUNT + calculateAdditionalDiscount(day);
+		int benefitAmount = calculateAdditionalDiscount(day);
 
 		return new Benefit(EVENT, benefitAmount * -1);
 	}
 
 	private int calculateAdditionalDiscount(int day) {
 		if (day >= MIN_DAY && day <= MAX_DAY) {
-			return ((day - MIN_DAY) * D_DAY_DISCOUNT);
+			return D_DAY_BASIC_DISCOUNT_AMOUNT + ((day - MIN_DAY) * D_DAY_DISCOUNT);
 		}
 
-		return MAX_DISCOUNT_AMOUNT;
+		return 0;
 	}
 }
