4b83544947dc9dca3ec77dc0b14f06ecb8ca64d4
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-11 17:23:10 +0900

    feat(OutputView): 주문 메뉴를 출력하는 기능 추가

---
+++ src/main/java/christmas/view/output/OutputView.java
@@ -0,0 +1,39 @@
+package christmas.view.output;
+
+import java.util.Map;
+import java.util.StringJoiner;
+
+import christmas.domain.Food;
+import christmas.domain.VisitDate;
+
+public class OutputView {
+
+	private static final String NEW_LINE = System.lineSeparator();
+
+	public void printGreeting() {
+		System.out.println(OutputViewMessage.GREETING.getMessage());
+	}
+
+	public void printEventPreview(VisitDate visitDate) {
+		int day = visitDate.getDay();
+		String message = String.format(OutputViewMessage.EVENT_PREVIEW.getMessage(), day);
+
+		System.out.println(message);
+	}
+
+	public void printOrderMenu(Map<Food, Integer> foods) {
+		StringJoiner orderMenuMessage = new StringJoiner(System.lineSeparator());
+		orderMenuMessage.add(OutputViewMessage.ORDER_MENU.getMessage());
+
+		for (Map.Entry<Food, Integer> entry : foods.entrySet()) {
+			String foodName = entry.getKey().name();
+			int number = entry.getValue();
+
+			orderMenuMessage.add(String.format("%s %d개", foodName, number));
+		}
+
+		System.out.println(NEW_LINE + orderMenuMessage.toString());
+	}
+
+	
+}
