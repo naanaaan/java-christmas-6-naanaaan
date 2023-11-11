be9e704ca0645c3c1da9e8a51dbaa6a9dcd8caef
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 04:08:53 +0900

    refactor(SpeciallDiscountServiceTest): 할인금액을 확인하는 기능 파라미터 추가

--- src/test/java/christmas/service/benefit/SpeciallDiscountServiceTest.java
+++ src/test/java/christmas/service/benefit/SpeciallDiscountServiceTest.java
@@ -17,11 +17,11 @@ public class SpeciallDiscountServiceTest {
 	}
 
 	@DisplayName("특별 할인 날짜일 때 할인금액을 확인한다.")
-	@CsvSource(value = { "3,-1000", "17,-1000", "31,-1000" }, delimiter = ',')
+	@CsvSource(value = { "3,-1000", "17,-1000", "31,-1000", "1,0" }, delimiter = ',')
 	@ParameterizedTest
 	void checkDiscountAmount(int day, int expect) {
 		int discountAmount = specialDiscountService.getBenefit(day).benefitAmount();
 
-		assertEquals(discountAmount, expect);
+		assertEquals(expect, discountAmount);
 	}
 }
