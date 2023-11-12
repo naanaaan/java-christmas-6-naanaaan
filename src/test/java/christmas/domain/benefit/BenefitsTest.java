d5429071d9e801a36a74f061039d43b5d8347af5
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-12 22:43:01 +0900

    refactor(BenefitsTest): 혜택금액이 0이 아닌 혜택들을 반환하는 지 확인하는 테스트 코드 수정

--- src/test/java/christmas/domain/benefit/BenefitsTest.java
+++ src/test/java/christmas/domain/benefit/BenefitsTest.java
@@ -8,26 +8,19 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import christmas.domain.benefit.Benefit;
-import christmas.domain.benefit.Benefits;
-import christmas.domain.benefit.DecemberEvent;
-
 class BenefitsTest {
 
-	@DisplayName("할인가격이 0이 아닌 혜택들을 반환하는지 확인한다.")
+	@DisplayName("헤택금액이이 0이 아닌 혜택들을 반환하는지 확인한다.")
 	@Test
-	void checkNotZeroDiscountAmountBenefits() {
+	void checkNotZeroBenefitAmountBenefits() {
 		Benefit benefit1 = new Benefit(DecemberEvent.DDAY_DISCOUNT, -1000);
 		Benefit benefit2 = new Benefit(DecemberEvent.WEEKDAY_DISCOUNT, 0);
 		Benefit benefit3 = new Benefit(DecemberEvent.GIVEAWAY_EVENT, -10000);
 
-		Benefits benefitsObject = new Benefits(List.of(benefit1, benefit2, benefit3));
+		Benefits benefits = new Benefits(List.of(benefit1, benefit2, benefit3));
 
-		List<Benefit> result = benefitsObject.getNotZeroDiscountAmount();
-
-		assertEquals(2, result.size());
-		assertTrue(result.contains(benefit1));
-		assertTrue(result.contains(benefit3));
-
+		assertEquals(2, benefits.getSize());
+		assertTrue(benefits.toList().contains(benefit1));
+		assertTrue(benefits.toList().contains(benefit3));
 	}
 }
