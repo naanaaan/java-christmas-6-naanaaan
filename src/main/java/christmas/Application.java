b95a45690ca77371ac9407956ab3b83dc8a13dd1
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-13 17:15:10 +0900

    feat(Application): 앱 실행

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,8 @@
 package christmas;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		ChristmasPromotion christmasPromotion = new ChristmasPromotion();
+		christmasPromotion.run();
+	}
 }
