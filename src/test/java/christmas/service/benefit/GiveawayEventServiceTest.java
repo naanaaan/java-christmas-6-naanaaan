84507c6c0986f4474509a89d74721e53df7c1ddf
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-13 15:32:48 +0900

    test(GiveawayEventService): getBenefit메서드의 테스트 코드 추가

--- src/test/java/christmas/service/benefit/GiveawayEventServiceTest.java
+++ src/test/java/christmas/service/benefit/GiveawayEventServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import christmas.domain.benefit.Benefit;
+import christmas.domain.benefit.DecemberEvent;
 import christmas.domain.food.Food;
 import christmas.domain.food.FoodMenu;
 
@@ -16,6 +18,15 @@ public class GiveawayEventServiceTest {
 	@BeforeEach
 	void setUp() {
 		giveawayEventService = new GiveawayEventService();
+	}
+
+	@DisplayName("증정헤택의 이벤트와 헤택금액을 확인한다.")
+	@Test
+	void checkGiveawayBenefitEventAndAmount() {
+		Benefit benefit = giveawayEventService.getBenefit();
+
+		assertEquals(DecemberEvent.GIVEAWAY_EVENT, benefit.event());
+		assertEquals(FoodMenu.CHAMPAGNE.getPrice(), benefit.benefitAmount());
 	}
 
 	@DisplayName("증정품이 샴페인인지 확인한다.")
