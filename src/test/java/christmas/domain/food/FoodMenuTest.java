87fb1d431cc6a229cddb8b6df3ee24d8955176b9
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-13 15:11:10 +0900

    test(FoodMenu): 메뉴에 있는 음식이름인지 확인하는 기능 테스트 코드 추가

--- src/test/java/christmas/domain/food/FoodMenuTest.java
+++ src/test/java/christmas/domain/food/FoodMenuTest.java
@@ -9,9 +9,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import christmas.domain.food.FoodMenu;
-
 public class FoodMenuTest {
+
+	@DisplayName("Test")
+	@CsvSource(value = { "초코케이크,true", "족발,false", "치킨,false", "시저샐러드,true", "레드와인,true" }, delimiter = ',')
+	@ParameterizedTest
+	void checkNameByMenuExists(String name, boolean expect) {
+		boolean result = FoodMenu.checkByName(name);
+
+		assertEquals(expect, result);
+	}
 
 	@DisplayName("메뉴 이름에 해당하는 Enum 객체를 확인한다.")
 	@CsvSource(value = { "양송이수프, MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA",
