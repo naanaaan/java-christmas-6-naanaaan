f5b0327c93dd2eb2c7fdcd3421d320a797b6d390
'sun1234' <'sdfjikl8787@gmail.com'>
2023-11-14 04:20:02 +0900

    refactor(FoodMenuTest): 수정된 FoodMenu의 상수값에 맞게 파라미터 수정

--- src/test/java/christmas/domain/food/FoodMenuTest.java
+++ src/test/java/christmas/domain/food/FoodMenuTest.java
@@ -21,7 +21,7 @@ public class FoodMenuTest {
 	}
 
 	@DisplayName("메뉴 이름에 해당하는 Enum 객체를 확인한다.")
-	@CsvSource(value = { "양송이수프, MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA",
+	@CsvSource(value = { "양송이수프, BUTTON_MUSHROOM_SOUP", "티본스테이크, T_BONE_STEAK", "크리스마스파스타, CHRISTMAS_PASTA",
 			"초코케이크, CHOCO_CAKE", "아이스크림, ICE_CREAM", "제로콜라, ZERO_COLA" }, delimiter = ',')
 	@ParameterizedTest
 	void checkGetMenuByName(String menuName, FoodMenu expect) {
